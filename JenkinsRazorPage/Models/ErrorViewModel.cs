ceae17afabdd054ecd77675e73c564d94fde71c4
madhavanram120@gmail.com <madhavanram120@gmail.com>
2025-08-25 10:09:48 +0530

    initial commit

---
+++ JenkinsRazorPage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JenkinsRazorPage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
